773ccc29d5f9107874cc13d56edb4629deb82f3e
namng <nllsover8.0@gmail.com>
2025-01-07 02:39:08 +0700

    chore: trigger cicd

--- src/main/java/com/normdevstorm/commerce_platform/CommercePlatformApplication.java
+++ src/main/java/com/normdevstorm/commerce_platform/CommercePlatformApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CommercePlatformApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CommercePlatformApplication.class, args);
 	}
