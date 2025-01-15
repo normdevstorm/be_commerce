75dcd1e5c4ebe3ea3878424b7b3e04bee4e87e33
namng <nllsover8.0@gmail.com>
2025-01-15 13:56:45 +0700

    chore: allow normdevstorm.online domain

--- src/main/java/com/normdevstorm/commerce_platform/config/api/SwaggerConfig.java
+++ src/main/java/com/normdevstorm/commerce_platform/config/api/SwaggerConfig.java
@@ -7,7 +7,12 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition
@@ -19,5 +24,12 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT",
         in = SecuritySchemeIn.HEADER)
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://api.normdevstorm.online/normdevstorm");
+        return new OpenAPI().servers(List.of(server));
+    }
 
 }
